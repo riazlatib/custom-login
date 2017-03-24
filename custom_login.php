b4be8b3925f75b6f0fea3343b221538f6b8e1ae6
Riaz Latib <riazlatib@gmail.com>
2017-03-23 21:14:33 -0400

    Basic Setup

--- custom_login.php
+++ custom_login.php
@@ -8,4 +8,9 @@ Author: Riaz Latib
 Author URI: http://riazlatib.com
 License: GPL2
 */
+
+function my_custom_login() {
+  echo '<link rel="stylesheet" type="text/css" href="' . plugin_dir_url( __FILE__ ) . 'css/custom-login-styles.css" />';
+}
+add_action('login_head', 'my_custom_login');
 ?>
